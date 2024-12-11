077bdeb2cacb2edbbc226499201e2f06743b2ec0
Prathamesh J. Pawar <prathmeshpawar212002@gmail.com>
2024-12-11 23:20:08 +0530

    FInal Deploy to vercel

--- app.py
+++ app.py
@@ -123,9 +123,9 @@ def process_image():
         # issue with openCV Solved for Hosting Project - Memory leckage Problem in RAM 
         gc.collect()
 
-if __name__ == '__main__':
-    try:
-        # app.run(host='0.0.0.0', port=8000)  # for gunicorn deployment purpose
-        app.run(debug=True , host='0.0.0.0', port=5000)  # for Localhost
-    except Exception as e:
-        logging.error(f"Error running app: {e}")
+# if __name__ == '__main__':
+#     try:
+#         # app.run(host='0.0.0.0', port=8000)  # for gunicorn deployment purpose
+#         app.run(debug=True , host='0.0.0.0', port=5000)  # for Localhost
+#     except Exception as e:
+#         logging.error(f"Error running app: {e}")
